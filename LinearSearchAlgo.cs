24cc778ce4f9a6133804d7fb6f9d630af1db18b5
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-29 00:29:50 +0100

    Add project files.

---
+++ LinearSearchAlgo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingAlgo
+{
+    public class LinearSearchAlgo
+    {
+        public static int LinearSearch(int [] arr, int k)
+        {
+            int n = arr.Length;
+            int i = 0;
+            while (i < n)
+            {
+                if (arr[i] == k)
+                {
+                    return i;
+
+                }
+                i++;
+            }
+            return -1;
+        }
+
+        public static string LinearSearch(List<int> list, int l)
+        {
+            int n = list.Count;
+            for (int i = 0; i < n; i++)
+            {
+                if (list[i] == l)
+                {
+                    return $"the index of the item to search for is {i}";
+                }
+
+            }
+            return $"the item to search for is not found";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SearchingAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 0;
+            var res = LinearSearchAlgo.LinearSearch(arr, k);
+            Console.WriteLine(res);
+
+            Console.WriteLine("########################");
+            List<int> list = new() { 1, 2, 3, 4, 5, 6, 7 };
+            int l = 10;
+            var listRes = LinearSearchAlgo.LinearSearch(list, l);
+            Console.WriteLine(listRes);
+        }
+    }
+}
